5b9ea98730ebfc0a062af1f9f43f365938760bab
Zawadi Kiarie <zawadikiarie@gmail.com>
2024-10-01 10:43:10 +0300

    Add fav icons

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,8 @@ const SearchBox = ({ searchChange }) => {
   console.log('SearchBox')
   return (
     <div className='pa2'>
-      <input 
+      <input
+        aria-label='Search Robots' 
         className = 'pa3 ba b--green bg-lightest-blue'
         type="search" 
         placeholder='search robots'
